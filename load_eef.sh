c9e2a12deb76a3cf75f8b99545bb13e9006a60d8
hredan <hredan@sourcecode3d.de>
2024-07-12 22:32:52 +0200

    final payloads for linux and mac OS

--- load_eef.sh
+++ load_eef.sh
@@ -10,11 +10,24 @@ ARCHIVE_WIN64="espeasyflasher_win64.zip"
 ARCHIVE_LINUX64="ESPEasyFlasher_linux_x64.tar.gz"
 ARCHIVE_LINUXARM64="ESPEasyFlasher_linux_arm64.tar.gz"
 ARCHIVE_LINUXARMV7="ESPEasyFlasher_linux_armv7.tar.gz"
-ARCHIVE_MACOS12="ESPEasyFlasher_macos12.tar.gz"
-ARCHIVE_MACOS14="ESPEasyFlasher_macos14.tar.gz"
+ARCHIVE_MACOS12="ESPEasyFlasher_macOs12_intel.tar.gz"
+ARCHIVE_MACOS14="ESPEasyFlasher_macOs14_arm64.tar.gz"
 
-print_something () {
-    echo Hello $1
+ARCHIVES="./Archives"
+TEMP_DIR="./Temp"
+WITH_PAYLOAD="./WithPayload"
+
+## fuction to download and extract the archive
+downloadExtract () {
+    mkdir -p $ARCHIVES
+    LINK="https://github.com/hredan/ESPEASYFLASHER_2.0/releases/download/${EEF_VERSION}/$1"
+    # curl -kLSs $LINK -o ./Targets/$1 # -k: allow insecure server connections when using SSL -s: silent mode -S: show error
+    if [ ! -f ${ARCHIVES}/$1 ]; then
+        curl -L $LINK -o ${ARCHIVES}/$1
+    fi
+    mkdir -p $TEMP_DIR
+    rm -rf $TEMP_DIR/*
+    tar -xzf $ARCHIVES/$1 -C $TEMP_DIR
 }
 
 while getopts t:h flag
@@ -30,17 +43,40 @@ if [ -z $TARGET ]; then
     exit 1
 fi
 
+mkdir -p $WITH_PAYLOAD
+mkdir -p $WITH_PAYLOAD
+
 if [ $TARGET = "linux64" ] || [ $TARGET = "all" ]; then
     echo "linux64 payload"
-    print_something "linux64"
-    # LINK="https://github.com/hredan/ESPEASYFLASHER_2.0/releases/download/${EEF_VERSION}/${EEF_ARCHIVE_NAME_LINUX64}"
-    # curl -kLSs $LINK_LINUX64 -o $EEF_ARCHIVE_NAME_LINUX64
-
-    # mkdir ./ESP_Packages
-    # cp *.eep ./ESP_Packages
-    # tar -xzf $EEF_ARCHIVE_NAME_LINUX64 ./ESP_Packages/*.*
+    downloadExtract $ARCHIVE_LINUX64
+    cp ./Artifacts/*.eep $TEMP_DIR/ESP_Packages
+    tar -czf ${WITH_PAYLOAD}/${ARCHIVE_LINUX64} -C $TEMP_DIR .
 fi
 
+if [ $TARGET = "linuxarm64" ] || [ $TARGET = "all" ]; then
+    echo "linuxarm64 payload"
+    downloadExtract $ARCHIVE_LINUXARM64
+    cp ./Artifacts/*.eep $TEMP_DIR/ESPEasyFlasher/ESP_Packages
+    tar -czf ${WITH_PAYLOAD}/${ARCHIVE_LINUXARM64} -C $TEMP_DIR .
+fi
 
+if [ $TARGET = "linuxarmv7" ] || [ $TARGET = "all" ]; then
+    echo "linuxarmv7 payload"
+    downloadExtract $ARCHIVE_LINUXARMV7
+    cp ./Artifacts/*.eep $TEMP_DIR/ESPEasyFlasher/ESP_Packages
+    tar -czf ${WITH_PAYLOAD}/${ARCHIVE_LINUXARMV7} -C $TEMP_DIR .
+fi
 
+if [ $TARGET = "macos12" ] || [ $TARGET = "all" ]; then
+    echo "macos12 payload"
+    downloadExtract $ARCHIVE_MACOS12
+    cp ./Artifacts/*.eep $TEMP_DIR/ESPEasyFlasher.app/Contents/MacOS/ESP_Packages
+    tar -czf ${WITH_PAYLOAD}/${ARCHIVE_MACOS12} -C $TEMP_DIR .
+fi
 
+if [ $TARGET = "macos14" ] || [ $TARGET = "all" ]; then
+    echo "macos14 payload"
+    downloadExtract $ARCHIVE_MACOS14
+    cp ./Artifacts/*.eep $TEMP_DIR/ESPEasyFlasher.app/Contents/MacOS/ESP_Packages
+    tar -czf ${WITH_PAYLOAD}/${ARCHIVE_MACOS14} -C $TEMP_DIR .
+fi
