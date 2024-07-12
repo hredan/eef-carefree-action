3c472efa85211d68032381261e679af5a7ff8e4d
hredan <hredan@sourcecode3d.de>
2024-07-12 07:58:03 +0200

    save work

--- load_eef.sh
+++ load_eef.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/bash
+
+EEF_VERSION="v1.0.1"
+
 HELP="Paramter:\n
 -t\tTARGET all/win64/linux64/linuxarm64/linuxarmv7/macos12/macos14\n
 e.g. ./eef_payload.sh -t all\n"
 
-EEF_VERSION="v1.0.1"
-EEF_ARCHIVE_NAME_WIN64="espeasyflasher_win64.zip"
-EEF_ARCHIVE_NAME_LINUX64="ESPEasyFlasher_linux_x64.tar.gz"
+ARCHIVE_WIN64="espeasyflasher_win64.zip"
+ARCHIVE_LINUX64="ESPEasyFlasher_linux_x64.tar.gz"
+ARCHIVE_LINUXARM64="ESPEasyFlasher_linux_arm64.tar.gz"
+ARCHIVE_LINUXARMV7="ESPEasyFlasher_linux_armv7.tar.gz"
+ARCHIVE_MACOS12="ESPEasyFlasher_macos12.tar.gz"
+ARCHIVE_MACOS14="ESPEasyFlasher_macos14.tar.gz"
 
-LINK_WIN64="https://github.com/hredan/ESPEASYFLASHER_2.0/releases/download/${EEF_VERSION}/${EEF_ARCHIVE_NAME_WIN64}"
-LINK_LINUX64="https://github.com/hredan/ESPEASYFLASHER_2.0/releases/download/${EEF_VERSION}/${EEF_ARCHIVE_NAME_LINUX64}"
+print_something () {
+    echo Hello $1
+}
 
 while getopts t:h flag
 do
@@ -25,7 +32,10 @@ fi
 
 if [ $TARGET = "linux64" ] || [ $TARGET = "all" ]; then
     echo "linux64 payload"
+    print_something "linux64"
+    # LINK="https://github.com/hredan/ESPEASYFLASHER_2.0/releases/download/${EEF_VERSION}/${EEF_ARCHIVE_NAME_LINUX64}"
     # curl -kLSs $LINK_LINUX64 -o $EEF_ARCHIVE_NAME_LINUX64
+
     # mkdir ./ESP_Packages
     # cp *.eep ./ESP_Packages
     # tar -xzf $EEF_ARCHIVE_NAME_LINUX64 ./ESP_Packages/*.*
