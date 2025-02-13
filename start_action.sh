6fc8cbf7b8585c8b602bc7de01a9410702b1a633
hredan <hredan@sourcecode3d.de>
2025-02-13 20:54:38 +0100

    Workflow: get path of action script

--- start_action.sh
+++ start_action.sh
@@ -3,6 +3,8 @@ SCRIPT_DIR=${0%/*}
 PARAMS=""
 
 echo Target: $INPUT_TARGET
+echo Script dir: $SCRIPT_DIR
+echo $PWD
 
 # run build_sketch.sh with parameters
-$SCRIPT_DIR/load_eef.sh $INPUT_TARGET
+# $SCRIPT_DIR/load_eef.sh $INPUT_TARGET
