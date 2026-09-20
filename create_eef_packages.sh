c7dc7371e5cbc4eeb259ea04947c35f54717bc71
hredan <hredan@sleepuino.de>
2026-09-20 07:26:48 +0200

    bugfix: linux_x64 Logo and Config not in executable folder

--- create_eef_packages.sh
+++ create_eef_packages.sh
@@ -111,7 +111,7 @@ if [ $TARGET = "linux_x64" ] || [ $TARGET = "all" ]; then
     echo "linux64 payload"
     downloadExtractGz $ARCHIVE_LINUX64
     cp ./$EEP_PACKAGE_DIR/*.eep $TEMP_DIR/ESP_Packages
-    addLogoAndConfig $TEMP_DIR/ESP_Packages
+    addLogoAndConfig $TEMP_DIR
     tar -czf ${EEF_PACKAGE_DIR}/${BASE_NAME_LINUX64}_${NAME}.tar.gz -C $TEMP_DIR .
 fi
 
